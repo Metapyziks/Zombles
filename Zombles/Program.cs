635838b07bad36832f1391205a8bca21a80cae14
James King <metapyziks@gmail.com>
2012-05-03 15:12:35 +0100

    Initial commit

---
+++ Zombles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zombles
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Elhlo Orwld!" );
+        }
+    }
+}
