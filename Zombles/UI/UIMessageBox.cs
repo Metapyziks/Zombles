c87022802c32aa2d0798fb7ae70131ca207d071c
James King <metapyziks@gmail.com>
2012-05-03 16:35:59 +0100

    Added UI code

---
+++ Zombles/UI/UIMessageBox.cs
@@ -0,0 +1,58 @@
+using System;
+
+using OpenTK;
+
+namespace Zombles.UI
+{
+    public class UIMessageBox : UIWindow
+    {
+        private UILabel myText;
+        private bool myCentreText;
+
+        public bool CentreText
+        {
+            get
+            {
+                return myCentreText;
+            }
+            set
+            {
+                myCentreText = value;
+                if ( myCentreText )
+                    myText.Centre();
+                else
+                    myText.Position = new Vector2( 4, 4 );
+            }
+        }
+
+        public String Text
+        {
+            get
+            {
+                return myText.Text;
+            }
+            set
+            {
+                myText.Text = value;
+                if( CentreText )
+                    myText.Centre();
+            }
+        }
+
+        public UIMessageBox( String message, String title, bool closeButton = true )
+            : base( new Vector2( 480, 64 ) )
+        {
+            CanClose = closeButton;
+            myCentreText = false;
+
+            Title = title;
+
+            myText = new UILabel( Zombles.Graphics.Font.Large )
+            {
+                Text = message,
+                Position = new Vector2( 4, 4 )
+            };
+            AddChild( myText );
+        }
+    }
+}
---
+++ Zombles/UI/UIWindowCloseButton.cs
@@ -0,0 +1,51 @@
+using ResourceLib;
+
+using OpenTK;
+
+using Zombles.Graphics;
+
+namespace Zombles.UI
+{
+    public class UIWindowCloseButton : UIObject
+    {
+        private Sprite mySprite;
+
+        public UIWindowCloseButton( float scale = 1.0f )
+            : this( new Vector2(), scale )
+        {
+        
+        }
+
+        public UIWindowCloseButton( Vector2 position, float scale = 1.0f )
+            : base( new Vector2(), position )
+        {
+            Texture2D texture = Res.Get<Texture2D>( "images_gui_panels" );
+            mySprite = new Sprite( texture, scale )
+            {
+                SubrectOffset = new Vector2( 32, 0 ),
+                SubrectSize = new Vector2( 16, 16 )
+            };
+
+            SetSize( 16.0f * scale, 16.0f * scale );
+
+            CanResize = false;
+        }
+
+        protected override void OnMouseEnter( Vector2 mousePos )
+        {
+            mySprite.SubrectLeft = 48.0f;
+        }
+
+        protected override void OnMouseLeave( Vector2 mousePos )
+        {
+            mySprite.SubrectLeft = 32.0f;
+        }
+
+        protected override void OnRender( SpriteShader shader, Vector2 renderPosition = new Vector2() )
+        {
+            mySprite.Position = renderPosition;
+
+            mySprite.Render( shader );
+        }
+    }
+}
