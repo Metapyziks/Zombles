908c89893d874fc661f3b5dcedea448708730db8
James King <metapyziks@gmail.com>
2012-05-07 14:34:14 +0100

    Added a DistrictEnumerator class

---
+++ Zombles/Geometry/DistrictEnumerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zombles.Geometry
+{
+    public class DistrictEnumerator : IEnumerator<Block>
+    {
+        private District myRootDistrict;
+        private District myCurDistrict;
+
+        public Block Current
+        {
+            get { return myCurDistrict.Block; }
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public DistrictEnumerator( District root )
+        {
+            myRootDistrict = root;
+            myCurDistrict = null;
+        }
+
+        public void Dispose()
+        {
+            myCurDistrict = null;
+            myRootDistrict = null;
+        }
+
+        public bool MoveNext()
+        {
+            if ( myCurDistrict == null )
+                myCurDistrict = myRootDistrict;
+            else if ( myCurDistrict.IsLeaf )
+            {
+                while( !myCurDistrict.IsRoot && myCurDistrict == myCurDistrict.Parent.ChildB )
+                    myCurDistrict = myCurDistrict.Parent;
+
+                if ( myCurDistrict.IsRoot )
+                    return false;
+
+                myCurDistrict = myCurDistrict.Parent.ChildB;
+            }
+
+            while ( myCurDistrict.IsBranch )
+                myCurDistrict = myCurDistrict.ChildA;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            myCurDistrict = null;
+        }
+    }
+}
