d6d9c836b3db1e6ed35fdc649c295ea6f5c586e0
James King <metapyziks@gmail.com>
2013-11-19 15:03:47 +0000

    Removed some unused usings

--- Scripts/Geometry/Generation/Rooms/Warehouse.cs
+++ Scripts/Geometry/Generation/Rooms/Warehouse.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Zombles.Geometry;
 using Zombles.Geometry.Generation;
 
