ad86ef33c5429ad1323d49e2120e74e7827e0143
Metapyziks <metapyziks@gmail.com>
2014-02-06 14:51:18 +0000

    Created component class for BDI AI

---
+++ Scripts/Entities/DeliberativeAI.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+
+using Zombles.Entities;
+using Zombles.Geometry;
+
+namespace Zombles.Scripts.Entities
+{
+    public class DeliberativeAI : HumanControl
+    {
+        public DeliberativeAI(Entity ent)
+            : base(ent)
+        {
+
+        }
+
+        public override void OnSpawn()
+        {
+            base.OnSpawn();
+        }
+
+        public override void OnThink(double dt)
+        {
+            base.OnThink(dt);
+        }
+    }
+}
