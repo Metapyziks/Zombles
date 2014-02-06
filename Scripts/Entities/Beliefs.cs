37f96908aae7a690bf77fce78c4c2cbaa6734036
Metapyziks <metapyziks@gmail.com>
2014-02-06 15:39:07 +0000

    Added outline for block-level beliefs

---
+++ Scripts/Entities/Beliefs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zombles.Geometry;
+
+namespace Zombles.Scripts.Entities
+{
+    internal sealed class Beliefs
+    {
+        private class BlockBeliefs
+        {
+            public double LastSeen { get; private set; }
+
+            public Block Block { get; private set; }
+
+            public int Survivors { get; private set; }
+
+            public int Zombies { get; private set; }
+
+            public int Resources { get; private set; }
+
+            public bool HasBuilding { get; private set; }
+
+            public double Utility { get; private set; }
+        }
+    }
+}
