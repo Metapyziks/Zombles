4108430a70bd5e13dc86345ddca2b74ec41257fe
Metapyziks <metapyziks@gmail.com>
2014-02-11 21:42:11 +0000

    More BDI

--- Scripts/Entities/Beliefs.cs
+++ Scripts/Entities/Beliefs.cs
@@ -11,9 +11,22 @@ namespace Zombles.Scripts.Entities
 {
     internal sealed class Beliefs
     {
+        private enum EntityType
+        {
+            Other = 0,
+            Survivor = 1,
+            Zombie = 2,
+            PlankPile = 3,
+            PlankSource = 4
+        }
+
         private class EntityBeliefs
         {
             public Entity Entity { get; private set; }
+
+            public EntityType Type { get; private set; }
+
+            public Beliefs Beliefs { get; private set; }
 
             public Block LastBlock { get; private set; }
 
@@ -24,6 +37,11 @@ namespace Zombles.Scripts.Entities
             public EntityBeliefs(Entity ent)
             {
                 Entity = ent;
+                Type = ent.HasComponent<Survivor>() ? EntityType.Survivor
+                    : ent.HasComponent<Zombie>() ? EntityType.Zombie
+                    : ent.HasComponent<WoodPile>() ? EntityType.PlankPile
+                    : ent.HasComponent<WoodenBreakable>() ? EntityType.PlankSource
+                    : EntityType.Other;
 
                 Update();
             }
@@ -38,21 +56,56 @@ namespace Zombles.Scripts.Entities
 
         private class BlockBeliefs
         {
-            public double LastSeen { get; private set; }
+            private int _survivors;
+            private int _zombies;
+            private int _resources;
+
+            private double _utility;
+            private bool _utilityChanged;
 
             public Block Block { get; private set; }
 
-            public int Survivors { get; private set; }
+            public Beliefs Beliefs { get; private set; }
+            
+            public double LastSeen { get; private set; }
 
-            public int Zombies { get; private set; }
+            public int Survivors { get { return _survivors + Beliefs._entityKB.Count(x => x.Value.Type == EntityType.Survivor && x.Value.LastBlock == Block); } }
 
-            public int Resources { get; private set; }
+            public int Zombies { get { return _zombies + Beliefs._entityKB.Count(x => x.Value.Type == EntityType.Zombie && x.Value.LastBlock == Block); } }
 
-            public double Utility { get; private set; }
+            public int Resources
+            {
+                get
+                {
+                    return _resources + Beliefs._entityKB
+                        .Where(x => x.Value.Type == EntityType.PlankPile && x.Value.LastBlock == Block)
+                        .Sum(x => x.Key.GetComponent<WoodPile>().Count) + Beliefs._entityKB
+                        .Where(x => x.Value.Type == EntityType.PlankSource && x.Value.LastBlock == Block)
+                        .Select(x => x.Key.GetComponent<WoodenBreakable>())
+                        .Sum(x => x.MinPlanks + x.MaxPlanks) / 2;
+                }
+            }
+
+            public double Utility
+            {
+                get
+                {
+                    if (_utilityChanged) {
+                        _utilityChanged = false;
+
+                        int zombies = Zombies;
+
+                        _utility = Survivors / 2.0 + Resources / 6.0 - zombies * zombies;
+                    }
+
+                    return _utility; 
+                }
+            }
 
             public BlockBeliefs(Block block)
             {
                 Block = block;
+                _utilityChanged = true;
 
                 Update();
             }
@@ -60,14 +113,6 @@ namespace Zombles.Scripts.Entities
             public void Update()
             {
                 LastSeen = MainWindow.Time;
-
-                Survivors = Block.Count(x => x.HasComponent<Survivor>() && x.GetComponent<Health>().IsAlive);
-                Zombies = Block.Count(x => x.HasComponent<Zombie>() && x.GetComponent<Health>().IsAlive);
-                Resources = Block.Where(x => x.HasComponent<WoodPile>() && Block.World.GetTile(x.Position2D).IsInterior)
-                    .Sum(x => x.GetComponent<WoodPile>().Count) + Block.Where(x => x.HasComponent<WoodenBreakable>())
-                    .Select(x => x.GetComponent<WoodenBreakable>()).Sum(x => x.MinPlanks + x.MaxPlanks) / 2;
-
-                Utility = Survivors / 2 + Resources / 6 - Zombies * Zombies;
             }
         }
 
@@ -103,7 +148,12 @@ namespace Zombles.Scripts.Entities
 
             foreach (var ent in Agent.Block) {
                 var hp = ent.GetComponentOrNull<Health>();
-                if (hp != null && !hp.IsAlive) continue;
+                if (hp != null && !hp.IsAlive) {
+                    if (_entityKB.ContainsKey(ent)) {
+                        _entityKB.Remove(ent);
+                    }
+                    continue;
+                }
 
                 trace.Target = ent.Position2D;
 
@@ -114,9 +164,9 @@ namespace Zombles.Scripts.Entities
             }
 
             var old = _entityKB.Values.Where(x => MainWindow.Time - x.LastSeen > 10.0).ToArray();
+
             foreach (var beliefs in old) {
                 _entityKB.Remove(beliefs.Entity);
-                
             }
         }
     }
